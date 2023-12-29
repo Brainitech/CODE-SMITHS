c360c011ba527b25874896e18d98fb2bf840b6b2
Brainiac <116993889+Brainitech@users.noreply.github.com>
2023-12-29 18:53:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,51 +1,73 @@
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
-import pandas as pd
-import matplotlib.pyplot as plt
 import yfinance as yf
 import datetime as dt
+import plotly.express as px
 
-# Initialize the Dash app
 app = dash.Dash(__name__)
 
-# Function to fetch stock data
-def get_stock_data(ticker, start_date, end_date):
-    stock_data = yf.download(ticker, start=start_date, end=end_date)
-    return stock_data
-
-# Function to create a plot
-def create_plot(stock_data):
-    plt.figure(figsize=(10, 6))
-    plt.plot(stock_data['Close'], label='Closing Price')
-    plt.title('Stock Price Over Time')
-    plt.xlabel('Date')
-    plt.ylabel('Closing Price')
-    plt.legend()
-    return plt
-
-# Layout of the app
 app.layout = html.Div([
-    html.H1("Stock Price Visualization"),
-    
-    html.Label("Enter Stock Code:"),
-    dcc.Input(id='stock-input', type='text', value='MSFT'),
-
-    html.Label("Select Date Range:"),
-    dcc.DatePickerRange(
-        id='date-picker',
-        start_date=dt.date(2022, 1, 1),
-        end_date=dt.date.today(),
-        display_format='YYYY-MM-DD'
-    ),
-
-    html.Button('PLOT GRAPH', id='plot-button', n_clicks=0),
-
-    dcc.Graph(id='stock-plot'),
-
+    html.Div([
+        html.H1("Stock Price Visualization"),
+    ]),
+    html.Div([    
+        html.Label("Enter Stock Code:"),
+        dcc.Input(id='stock-input', type='text', value='MSFT'),
+        html.Button('Search', id='search-button', n_clicks=0),
+    ]),
+    html.Div([
+        html.Label("Select Date Range:"),
+        dcc.DatePickerRange(
+            id='date-picker',
+            start_date=dt.date(2023, 1, 1),
+            end_date=dt.date.today(),
+            display_format='YYYY-MM-DD'
+        ),
+        html.Button('PLOT GRAPH', id='plot-button', n_clicks=0),
+    ]),
+    html.Div([
+        html.Div([
+            html.Img(id='company-logo',src=''),
+            html.H1(id='name')
+        ], className='header'),
+        html.Div(
+            id="description", className='descrription_ticker'),
+    ]),
+    html.Div([
+        dcc.Graph(id='stock-plot')
+    ]),
 ])
 
-# Callback to update the plot based on user input
+def get_company_info(selected_stock):
+    company_info = yf.Ticker(selected_stock)
+    company_logo_url = company_info.info.get('logo_url', '')
+    company_name = company_info.info.get('longName', '')
+    company_description = company_info.info.get('longBusinessSummary', '')
+    return company_logo_url, company_name, company_description
+
+@app.callback(
+    [Output('company-logo', 'src'),
+     Output('name', 'children'),
+     Output('description', 'children')],
+    [Input('search-button', 'n_clicks')],
+    [State('stock-input', 'value')]
+)
+def update_company_info(n_clicks, selected_stock):
+    if n_clicks > 0:
+        company_logo_url, company_name, company_description = get_company_info(selected_stock)
+        return company_logo_url, company_name, company_description
+    # If the button has not been clicked, return default values
+    return '', '', ''
+
+def get_stock_data(ticker, start_date, end_date):
+    try:
+        stock_data = yf.download(ticker, start=start_date, end=end_date)
+        return stock_data
+    except Exception as e:
+        print(f"Error fetching stock data: {e}")
+        return None
+
 @app.callback(
     Output('stock-plot', 'figure'),
     [Input('plot-button', 'n_clicks')],
@@ -56,16 +78,13 @@ app.layout = html.Div([
 def update_plot(n_clicks, selected_stock, start_date, end_date):
     if n_clicks > 0:
         stock_data = get_stock_data(selected_stock, start_date, end_date)
-        plt = create_plot(stock_data)
+        if stock_data is not None:
+            fig = px.line(stock_data, x=stock_data.index, y='Close',
+                          labels={'x': 'Date', 'y': 'Closing Price'},
+                          title=f'{selected_stock} Stock Price Over Time')
+            return fig
+    # If the button has not been clicked or an error occurred, return an empty graph
+    return px.line()
 
-        return {'data': [{'x': stock_data.index, 'y': stock_data['Close'], 'type': 'line', 'name': 'Closing Price'}],
-                'layout': {'title': f'{selected_stock} Stock Price Over Time',
-                           'xaxis': {'title': 'Date'},
-                           'yaxis': {'title': 'Closing Price'}}}
-    else:
-        # If the button has not been clicked, return an empty graph
-        return {'data': [], 'layout': {}}
-
-# Start app
 if __name__ == '__main__':
     app.run_server(debug=True)
